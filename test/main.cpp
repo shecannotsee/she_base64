eca1fe297ccfbd0d89d6f8d09c50a3dd0f15484c
shecannotsee <shecannotsee@163.com>
2022-12-02 11:27:54 +0800

    feat:测试代码运行测试

---
+++ test/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by shecannotsee on 2022/12/2.
+//
+#include <iostream>
+int main() {
+  std::cout<<"sheBase64 test start.\n";
+
+  std::cout<<"sheBase64 test end.\n";
+  return 0;
+};
