0d6e7f47b86d875a48df8c0efa154a5dbd3fd722
shecannotsee <shecannotsee@163.com>
2022-12-02 11:33:52 +0800

    feat:添加部分注释

--- BuildDependencies.sh
+++ BuildDependencies.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 cd libraries
 mkdir googletest
 cd ..
 
+# Compile googletest and install it to the specified directory
 cd 3rd
 cd googletest
 mkdir build
@@ -10,6 +12,7 @@ cmake ..
 make -j7
 make DESTDIR=../../../libraries/googletest install
 
+# move googletest's include and lib
 cd ../../../libraries/googletest
 mv -f ./usr/local/include ./
 mv -f ./usr/local/lib ./
