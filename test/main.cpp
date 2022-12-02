e501599203bc60299f72c2fe2b487854b5304c5f
shecannotsee <shecannotsee@163.com>
2022-12-02 17:23:27 +0800

    feat:添加功能测试的正常功能测试框架

--- test/main.cpp
+++ test/main.cpp
@@ -2,9 +2,12 @@
 // Created by shecannotsee on 2022/12/2.
 //
 #include <iostream>
+
+#include "FunctionalTesting/Normal.h"
+
 int main() {
   std::cout<<"sheBase64 test start.\n";
-
+  FunctionalTesting::Normal::testStart();
   std::cout<<"sheBase64 test end.\n";
   return 0;
 };
