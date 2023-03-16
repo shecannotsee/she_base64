3fe52eeada65635be30aa3d5dce4d848e830cd49
shecannotsee <shecannotsee@163.com>
2023-03-16 17:04:43 +0800

    feat: 1.优化静态赋值方法,增加程序的性能 2.解码时候做更加安全的转换

--- src/she_base64.cpp
+++ src/she_base64.cpp
@@ -4,22 +4,23 @@
 
 #include "she_base64.h"
 #include <stdexcept>
+#include <iostream>
 
-const char* she_base64::base64 = {
+const char she_base64::base64[] = {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz"
     "0123456789"
     "-_"
 };
 
-const char* she_base64::safe_url_base64 = {
+const char she_base64::safe_url_base64[] = {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     "abcdefghijklmnopqrstuvwxyz"
     "0123456789"
     "-_"
 };
 
-unsigned char she_base64::pos_of_char(const unsigned char chr) {
+she_base64::byte she_base64::pos_of_char(const char& chr) {
   if      (chr >= 'A' && chr <= 'Z') return chr - 'A';
   else if (chr >= 'a' && chr <= 'z') return chr - 'a' + ('Z' - 'A')               + 1;
   else if (chr >= '0' && chr <= '9') return chr - '0' + ('Z' - 'A') + ('z' - 'a') + 2;
@@ -31,7 +32,7 @@ unsigned char she_base64::pos_of_char(const unsigned char chr) {
 
 std::string she_base64::encode(std::string data,BASE_TYPE base64_type) {
   int base64_tail_fill = data.size() % 3;
-  unsigned char zero = 0x00;
+  byte zero = 0x00;
   if ( base64_tail_fill == 1 ) {
     data += zero;
     data += zero;
@@ -47,26 +48,28 @@ std::string she_base64::encode(std::string data,BASE_TYPE base64_type) {
     base64_table_table = she_base64::safe_url_base64;
   }
 
-  std::string base64_str;
-  for (int i=0; i < data.size(); i=i+3 ) {
-    unsigned char a;
-    a = static_cast<unsigned char>(data[i])>>2;
-    unsigned char b;
-    b = static_cast<unsigned char>(data[i])<<6;
-    b = b>>2;
-    b += static_cast<unsigned char>(data[i+1])>>4;
-    unsigned char c;
-    c = static_cast<unsigned char>(data[i+1])<<4;
-    c = c>>2;
-    c += static_cast<unsigned char>(data[i+2])>>6;
-    unsigned char d;
-    d = static_cast<unsigned char>(data[i+2])<<2;
-    d = d>>2;
-    base64_str += base64_table_table[a];
-    base64_str += base64_table_table[b];
-    base64_str += base64_table_table[c];
-    base64_str += base64_table_table[d];
-  }
+  std::string base64_str(data.size()/3*4,'A');
+  int index = 0;
+  byte temp = 0x00;
+  for (int i = 0; i < data.size(); i += 3) {
+    // Data type needs to be converted first
+    temp = (static_cast<byte>(data[i]))>>2;
+    base64_str[index++] = base64_table_table[temp];
+
+    temp = (static_cast<byte>(data[i]))<<6;
+    temp = temp>>2;
+    temp += (static_cast<byte>(data[i+1]))>>4;
+    base64_str[index++] = base64_table_table[temp];
+
+    temp = (static_cast<byte>(data[i+1]))<<4;
+    temp = temp>>2;
+    temp += (static_cast<byte>(data[i+2]))>>6;
+    base64_str[index++] = base64_table_table[temp];
+
+    temp = (static_cast<byte>(data[i+2]))<<2;
+    temp = temp>>2;
+    base64_str[index++] = base64_table_table[temp];
+  };
 
   // Add end : =
   if ( base64_tail_fill == 1 ) {
@@ -90,28 +93,28 @@ std::string she_base64::decode(const std::string& data_base64) {
   int data_real_length = (data_base64.size()/4 )* 3 - tail_fill ;
 
   std::string data;
-  unsigned char temp = 0x00;
+  byte temp = 0x00;
   int index = 0;
-  for (auto ch : data_base64) {
+  for (int i = 0; i < data_base64.size() ; i++) {
     if (data.size() == data_real_length) {
       break;
     } else if (index == 0) {
-      temp += she_base64::pos_of_char(ch)<<2;
+      temp += static_cast<byte>(she_base64::pos_of_char(data_base64[i])<<2);
       index = 6;
     } else if (index == 6) {
-      temp += she_base64::pos_of_char(ch)>>4;
+      temp += static_cast<byte>(she_base64::pos_of_char(data_base64[i])>>4);
       data += temp;
       temp = 0x00;
-      temp = she_base64::pos_of_char(ch)<<4;
+      temp = static_cast<byte>(she_base64::pos_of_char(data_base64[i])<<4);
       index = 4;
     } else if ( index == 4 ) {
-      temp += she_base64::pos_of_char(ch)>>2;
+      temp += static_cast<byte>(she_base64::pos_of_char(data_base64[i])>>2);
       data += temp;
       temp = 0x00;
-      temp = she_base64::pos_of_char(ch)<<6;
+      temp = static_cast<byte>(she_base64::pos_of_char(data_base64[i])<<6);
       index = 2;
     } else if ( index == 2 ) {
-      temp += she_base64::pos_of_char(ch);
+      temp += static_cast<byte>(she_base64::pos_of_char(data_base64[i]));
       data += temp;
       temp = 0x00;
       index = 0;
