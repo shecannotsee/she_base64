954662095482aa6087d595621c81ea8919679dd1
shecannotsee <shecannotsee@163.com>
2023-03-16 15:00:51 +0800

    test:将测试用例细分

--- test/m1_she_base64_test.h
+++ test/m1_she_base64_test.h
@@ -12,31 +12,39 @@
 
 namespace m1_she_base64_test {
 
-TEST(Base64Test, Encoding) {
+TEST(Base64Test, encoding_common) {
   // 测试 Base64 编码函数
   EXPECT_EQ(she_base64::encode("Hello, world!"), "SGVsbG8sIHdvcmxkIQ==");
   EXPECT_EQ(she_base64::encode("12345"), "MTIzNDU=");
   EXPECT_EQ(she_base64::encode(""), "");
   EXPECT_EQ(she_base64::encode("The quick brown fox jumps over the lazy dog."), "VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4=");
+};
+TEST(Base64Test, encoding_special) {
   EXPECT_EQ(she_base64::encode("\x00\x01\x02"), "AAEC");
   EXPECT_EQ(she_base64::encode("\xFF\xFE\xFD"), "/v7+");
-}
+};
 
-TEST(Base64Test, Decoding) {
+
+TEST(Base64Test, decoding_common) {
   // 测试 Base64 解码函数
   EXPECT_EQ(she_base64::decode("SGVsbG8sIHdvcmxkIQ=="), "Hello, world!");
   EXPECT_EQ(she_base64::decode("MTIzNDU="), "12345");
   EXPECT_EQ(she_base64::decode(""), "");
   EXPECT_EQ(she_base64::decode("VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4="), "The quick brown fox jumps over the lazy dog.");
+};
+TEST(Base64Test, decoding_special) {
   EXPECT_EQ(she_base64::decode("AAEC"), "\x00\x01\x02");
   EXPECT_EQ(she_base64::decode("/v7+"), "\xFF\xFE\xFD");
   EXPECT_EQ(she_base64::decode("SGVsbG8sIHdvcmxkIQ"), ""); // 不完整的编码字符串
-}
+};
+TEST(Base64Test, decoding_error) {
+  EXPECT_EQ(she_base64::decode("SGVsbG8sIHdvcmxkIQ"), ""); // 不完整的编码字符串
+};
 
 int main(int argc,char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-}
+};
 
 };// m1_she_base64_test
 
