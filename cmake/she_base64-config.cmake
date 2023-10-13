14c3908d14f64f3abb3a4f99289412a078be6b79
shecannotsee <shecannotsee@163.com>
2023-10-13 14:54:15 +0800

    [build] standardization

--- cmake/she_base64-config.cmake
+++ cmake/she_base64-config.cmake
@@ -1,5 +1,10 @@
+# ${she_base64_FOUND}       : right get
+# ${she_base64_INCLUDE_DIR} : include
+# ${she_base64_LIBRARIES}   : lib
+# ${she_base64_LINK_TARGET} : link target
+
 set(she_base64 "she_base64")
 set(${she_base64}_FOUND "1")
 set(${she_base64}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
 set(${she_base64}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
-set(${she_base64}_LINK_DIRECTORIES  "she_base64")
+set(${she_base64}_LINK_TARGET   "she_base64")
