b46caa90029495698bf16af631be62f1c72899f3
shecannotsee <shecannotsee@163.com>
2023-10-13 13:17:41 +0800

    [build] add find_package config

---
+++ cmake/she_base64-config.cmake
@@ -0,0 +1,5 @@
+set(she_base64 "she_base64")
+set(${she_base64}_FOUND "1")
+set(${she_base64}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${she_base64}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${she_base64}_LINK_DIRECTORIES  "she_base64")
